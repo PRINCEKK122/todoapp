12570dc4139333ab8c417d93cfbd6a0629d3eb24
PRINCEKK122 <awuahkarikari12@gmail.com>
2023-03-14 00:44:44 +0000

    created index.html to render a list of todo items using dummy data

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "index.html",
+        data=[
+            {"description": "Todo 1"},
+            {"description": "Todo 2"},
+            {"description": "Todo 3"},
+            {"description": "Todo 4"},
+        ],
+    )
