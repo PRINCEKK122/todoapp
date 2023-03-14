ec4e784db1a3546101bffdf8cd65e10d39316591
PRINCEKK122 <awuahkarikari12@gmail.com>
2023-03-14 01:45:04 +0000

    creating a new todo

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:abc@localhost:5432/todoapp"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:abc@localhost:5432/todoapp"
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     __tablename__ = "todos"
@@ -11,10 +14,12 @@ class Todo(db.Model):
     description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f"<Todo {self.id} {self.description}"
+        return f"<Todo {self.id} {self.description}>"
+
 
 with app.app_context():
     db.create_all()
+
 
 @app.route("/")
 def index():
@@ -22,3 +27,18 @@ def index():
         "index.html",
         data=Todo.query.all(),
     )
+
+
+@app.route("/todos/create", methods=["POST"])
+def create_todo():
+    new_todo = request.form.get("description", None)
+
+    try:
+        todo = Todo(description=new_todo) # transient mode
+        # creating a session, which is the same as a transaction
+        db.session.add(todo)
+        # now we are ready to flush the newly created data
+        db.session.commit()
+    except:
+        db.session.rollback()
+    return redirect(url_for("index"))
